f9f530ed31b1056e5f9a59868e645d80c131c6f5
Sebastian Paluchiewicz <sebastian.paluchiewicz@gmail.com>
2022-08-08 22:47:39 +0200

    Release 4.2.0

--- swiftymocky.rb
+++ swiftymocky.rb
@@ -1,11 +1,12 @@
 class Swiftymocky < Formula
   desc "Framework for automatic mock generation. Adds a set of handy methods, simplifying testing. One of the best and most complete solutions, including generics support and much more."
   homepage "https://github.com/MakeAWishFoundation/SwiftyMocky"
-  url "https://github.com/MakeAWishFoundation/SwiftyMocky/archive/refs/tags/4.1.0.tar.gz"
-  sha256 "729a95f6ed232ca1f660b6e549649dd1e6b9b4960583434e5de64232b1e1a3e3"
+  url "https://github.com/MakeAWishFoundation/SwiftyMocky/archive/refs/tags/4.2.0.tar.gz"
+  sha256 "7966443004dc04ad6759a152c34ce3d049c69d7055d51a5c813e3a65667dfa1e"
   license "MIT"
 
   depends_on xcode: "13.0"
+  depends_on "sourcery"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
